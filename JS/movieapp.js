dfeaedad6f66a57125d00137d278b2a3600d4af6
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-03-01 11:04:48 -0600

    feat: functions done for add, edit, delete

--- JS/movieapp.js
+++ JS/movieapp.js
@@ -1,17 +1,69 @@
 "use strict"
-//loading
 
+//LOADING MESSAGE
 $(document).ready(function() {
     $('#loading').text('Loading...')
 });
 
-
+//MOVIE DATABASE URL
 const movieUrl = 'https://rhinestone-spiced-mare.glitch.me/movies'
 
-let movieDatabase = fetch(movieUrl)
-    .then(response => response.json())
-    .then(data => data)
-        .catch(console.error);
+//GET MOVIES DATABASE
+const getMovies = () => fetch(movieUrl)
+    .then(res => res.json())
+    .catch(console.error)
+
+getMovies().then(movies => console.log(movies));
+
+//GET MOVIE WITH ID
+const getMovie = id => fetch(`${movieUrl}/${id}`)
+    .then(res => res.json())
+    .catch(console.error)
+
+getMovie(1).then(movie => console.log(movie));
+
+//EDIT MOVIE FUNCTION
+const editMovie = movie => fetch(`${movieUrl}/${movie.id}`, {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(movie)
+})
+    .then(res => res.json())
+    .then(data => {console.log(`Success: edited${JSON.stringify(data)}`)})
+    .catch(console.error);
+
+//DELETE MOVIE FUNCTION
+const deleteMovie = id => fetch(`${movieUrl}/${id}`, {
+    method: 'DELETE',
+    headers: {
+        'Content-type': 'application/json'
+    },
+})
+    .then(res => res.json())
+    .then(() => {
+        console.log(`Success: deleted movie with id of ${id}`)})
+    .catch(console.error)
+
+// deleteMovie(4).then(console.log);
+
+
+// ADD MOVIE FUNCTION
+const addMovie = movie => fetch(`${movieUrl}`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(movie)
+})
+    .then(res => res.json())
+    .then(data => {console.log(`Success: created ${JSON.stringify(data)}`)
+    return data.id;
+    })
+    .catch(console.error)
+
+addMovie("Jacob").then(console.log);
 
 
 
@@ -41,7 +93,7 @@ function displayHtml(movieDatabase) {
 
     });
 }
-displayHtml(movieDatabase);
+// displayHtml();
 
 
 //------------------------------------------------------------------------
@@ -115,57 +167,24 @@ $("#movieForm").submit(function (event) {
 // })
 
 
-// const addMovie = (moviesDatabase) => {
-//     // e.preventDefault();
-//     moviesDatabase.then(movies => {
-//         let newObj = {};
-//         newObj.push({
+// $('#add-submit').click((e) => {
+//     const movieName = $('#add-movie-name').val();
+//     const movieRating = $("#add-rating-selection").val();
+//     fetch(movieUrl,{
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
 //             title: movieName,
 //             rating: movieRating
 //         })
-//         movies = movies.concat(newObj);
-//     })
-// }
-// addMovie(movieDatabase);
-
-// let movieName = $('#add-movie-name').val();
-// let movieRating = $("#add-rating-selection").val();
-//
-// const addMovie = () => fetch(movieUrl,{
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({
+//     }).then(console.log(JSON.stringify({
 //         title: movieName,
 //         rating: movieRating
-//     },
-//     console.log(movieRating))
+//     }))).catch(console.error)
+//     e.preventDefault();
+//     displayHtml();
 // })
-//     .then(res => res.json())
-//     .then(data => console.log(data))
-//     .catch(console.error)
-
-
-
-$('#add-submit').click((e) => {
-    const movieName = $('#add-movie-name').val();
-    const movieRating = $("#add-rating-selection").val();
-    fetch(movieUrl,{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            title: movieName,
-            rating: movieRating
-        })
-    }).then(console.log(JSON.stringify({
-        title: movieName,
-        rating: movieRating
-    }))).catch(console.error)
-    e.preventDefault();
-    displayHtml();
-})
 
 
