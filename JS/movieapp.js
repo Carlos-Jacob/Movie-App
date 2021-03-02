26cf60fc103f92122b6b6d969767c3cfa8b658c5
Jacob-Valeriano <jacob.k.valeriano@gmail.com>
2021-03-01 20:29:12 -0600

    edit/open modal works

--- JS/movieapp.js
+++ JS/movieapp.js
@@ -85,9 +85,13 @@ function displayHtml() {
         }
         $('#loading').html(html)
 
+
+        // When edit button is clicked, modal button is clicked as well
+
         $('.editBtn').click(function () {
-            alert("clicked")
+            $('#myBtn').click()
         })
+
         // delete event is working but not deleting the specific ID
         $('.deleteBtn').click(function () {
             $(this).parent('div').remove();
@@ -158,6 +162,35 @@ $("#movieForm").submit(function (event) {
         }
     })
 })
+
+
+// Modal
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
 
